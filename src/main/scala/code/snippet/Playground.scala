862312f9f364d2567bfd33358fc0b316c9ca3da9
David Wasserstrum <davidwas@David-Wasserstrums-MacBook-Pro.local>
2011-04-25 10:55:37 -0400

    Validators and VHtml

--- src/main/scala/code/snippet/Playground.scala
+++ src/main/scala/code/snippet/Playground.scala
@@ -6,7 +6,7 @@ import net.liftweb.common._
 import java.util.Date
 import code.lib._
 import Helpers._
-import net.liftweb.http.{DispatchSnippet, S, SHtml}
+import net.liftweb.http.{DispatchSnippet, S, SHtml, WiringUI}
 import net.liftweb.http.js.JsCmds
 
 class Playground extends DispatchSnippet {
@@ -14,6 +14,7 @@ class Playground extends DispatchSnippet {
   val versions:List[(String, (NodeSeq => NodeSeq))] = List(
     ("basic", basic _),
     ("jasonsThoughts", jasonsThoughts _),
+    ("validators", validators _),
     ("yourVersion", yourVersion _)
   )
 
@@ -63,6 +64,85 @@ class Playground extends DispatchSnippet {
     "condition" -> NodeSeq.Empty,
     "submit" -> SHtml.submit("submit", () => if (optA.isDefined && optB.isDefined && (optA.get < optB.get)) (S.warning("good")) else (S.warning("bad"))))
   }
+
+
+  //Validators
+  //
+
+  //SimpleWiringUI makes WiringUI a pinch easier to use for the simple case.
+  object SimpleWiringUI{
+    def apply[T](dynamicValue: => T)(f: T => NodeSeq) = {
+      WiringUI(NodeSeq.Empty, DynamicCell(() => dynamicValue))(cellValue => ns => f(cellValue))
+    }
+  }
+
+
+  abstract class Validator {
+    def test(s: String): Boolean
+  }
+  object IntegerValidator extends Validator {
+    def test(s: String) = ControlHelpers.tryo(s.toInt).isDefined
+  }
+
+  //VHtml would replace SHtml and have similar methods (text, ajaxText, textArea, etc.)
+  //The "V" stands for Validator
+  object VHtml {
+    def ajaxText(value: String, errorMessage: String, validators: Validator*): VHtml = {
+      new VHtml() {
+        var varValue = value
+        def valueOpt = if (validatorsPass) Some(varValue) else None
+        private def validatorsPass = validators.forall(_.test(varValue))
+
+        def apply() = <span>{SHtml.ajaxText(varValue, varValue = _)}
+                          {SimpleWiringUI(varValue)(_ => if (validatorsPass) NodeSeq.Empty else Text(errorMessage))}
+                    </span>
+
+      }
+    }
+
+    def validatorMsg[T](value: => T, errorMessage: String, f: T => Boolean): VHtml = {
+      new VHtml() {
+        def valueOpt = if (f(value)) Some("") else None
+        def apply() = <span>{SimpleWiringUI(value)(currentValue => if (f(currentValue)) NodeSeq.Empty else Text(errorMessage))}</span>
+      }
+    }
+
+  }
+
+  abstract class VHtml() {
+    def apply(): NodeSeq
+    def valueOpt: Option[String]
+    //I like the idea below for Int, Calendar, Float, etc.  But how will it be extended for external types (JodaDateTime)?
+    def valueIntOpt: Option[Int] = valueOpt.flatMap(s => ControlHelpers.tryo(s.toInt))
+  }
+
+  val aVText = VHtml.ajaxText(a.toString, "must be an integer!", IntegerValidator)
+  val bVText = VHtml.ajaxText("", "must be an integer!", IntegerValidator)
+  val conditionText = VHtml.validatorMsg((aVText.valueIntOpt, bVText.valueIntOpt), "a must be less than b",
+  { a: (Option[Int], Option[Int]) => a match {
+    case ((x:Option[Int]), (y:Option[Int])) => x.exists(xv => y.exists(yv => xv < yv))
+    case _ => true
+  }
+  })
+
+  def validators(xhtml: NodeSeq): NodeSeq = {
+    bind("form", xhtml,
+    "item1" -> aVText(),
+    "item2" -> bVText(),
+    "condition" -> conditionText(),
+    "submit" -> SHtml.submit("submit", () => {
+      (for {
+        userA <- aVText.valueIntOpt
+        userB <- bVText.valueIntOpt
+        relationship <- conditionText.valueOpt //need to test the relationship between a and b
+      } yield {
+        a = userA
+        b = userB
+        S.warning("good")
+      }) getOrElse (S.warning("bad"))
+    }))
+  }
+
 
   //your version
   //
