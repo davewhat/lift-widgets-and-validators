782ee44142d39ce7a4b04c69eb679372b5c626e0
David Wasserstrum <davidwas@gmail.com>
2011-04-28 10:20:55 -0400

    Cleaning up "version picker"
    Originally thought we would have mulitple small snippets of code going in parallel.  Seems there is only once main code chunk being worked on, so killing all the bloat.

--- src/main/scala/code/snippet/Playground.scala
+++ src/main/scala/code/snippet/Playground.scala
@@ -9,83 +9,7 @@ import Helpers._
 import net.liftweb.http.{DispatchSnippet, S, SHtml, WiringUI}
 import net.liftweb.http.js.{JsCmd, JsCmds}
 
-class Playground extends DispatchSnippet {
-
-  val versions:List[(String, (NodeSeq => NodeSeq))] = List(
-    ("basic", basic _),
-    ("jasonsThoughts", jasonsThoughts _),
-    ("validators", validators _),
-    ("yourVersion", yourVersion _)
-  )
-
-  def dispatch : DispatchIt = {
-    case "render" if versions.exists( v => S.param("version").exists( v._1 == _)) => versions.find(_._1 == S.param("version").get).get._2
-    case "picker" => picker _
-    case x => x => NodeSeq.Empty
-  }
-  def picker(xhtml: NodeSeq): NodeSeq = {
-    versions.flatMap( v => <div><a href={"?version=%s" format v._1}>{v._1}</a> {if (S.param("version").exists(_ == v._1)) (<span>***</span>)}</div>)
-  }
-
-  //basic version
-  //
-
-  var a = 5
-  var b = 0
-
-  def basic(xhtml: NodeSeq): NodeSeq = {
-    bind("form", xhtml,
-    "item1" -> SHtml.ajaxText(a.toString, x => ControlHelpers.tryo(x.toInt) match {
-      case Full(y:Int) => a = y
-      case _ => S.error("a must be an Int")
-    }),
-    "item2" -> SHtml.text("", x => ControlHelpers.tryo(x.toInt) match {
-      case Full(y:Int) => b = y
-      case _ => S.error("b must be an Int")
-    }),
-    "condition" -> NodeSeq.Empty,
-    "submit" -> SHtml.submit("submit", () => {if (a<b) (S.warning("good")) else (S.error("bad"))}))
-  }
-
-  //jasonsThoughts -- My understanding of how Jason has done this in the past.
-  //
-  // -every field is wrapped with an Option[Field]
-  // -If all Options[Fields] are isDefined, we passed the field level tests
-  // -Any cross-field testing must be put into the submit
-  // -Negative: If we want to also warn that 'a is not less than b' we need duplicate logic (pop-up warning and submit button)
-
-  var optA:Option[Int] = Some(a)
-  var optB:Option[Int] = None
-  var validateAlessB: Option[Unit] = None
-
-  def jasonsThoughts(xhtml: NodeSeq): NodeSeq = {
-    bind("form", xhtml,
-    "item1" -> SHtml.ajaxText(a.toString, x => {optA = ControlHelpers.tryo(x.toInt); if (optA.isEmpty) (JsCmds.Alert("a must be an Int"))} ),
-    "item2" -> SHtml.ajaxText("", x => {optB = ControlHelpers.tryo(x.toInt); if (optB.isEmpty) (JsCmds.Alert("b must be an Int"))} ),
-    "condition" -> NodeSeq.Empty,
-    "alessb" -> SimpleWiringUI((optA, optB)) { _ =>
-      (for { a <- optA; b <- optB } yield {
-        validateAlessB = Some(()).filter(_ => a < b)
-	      validateAlessB match {
-          case Some(_) => NodeSeq.Empty
-          case None    => Text("a must be less than b")
-        }
-      }) getOrElse (NodeSeq.Empty)
-    },
-    "submit" -> SHtml.submit("submit", () => {
-      (for {
-        a <- optA
-        b <- optB
-        _ <- validateAlessB
-      } yield {
-        // save the values
-      }) getOrElse (S.error("Please correct the highlighted fields"))
-    }))
-  }
-
-
-  //Validators
-  //
+class Playground {
 
   //SimpleWiringUI makes WiringUI a pinch easier to use for the simple case.
   object SimpleWiringUI{
@@ -169,7 +93,7 @@ class Playground extends DispatchSnippet {
     }
   )
 
-  def validators(xhtml: NodeSeq): NodeSeq = {
+  def render(xhtml: NodeSeq): NodeSeq = {
     bind("form", xhtml,
     "item1" -> aVText.toForm,
     "item2" -> bVText.toForm,
@@ -181,17 +105,4 @@ class Playground extends DispatchSnippet {
       }
     }))
   }
-
-
-  //your version
-  //
-
-  def yourVersion(xhtml: NodeSeq): NodeSeq = {
-    bind("form", xhtml,
-    "item1" -> SHtml.text(a.toString, x => a = x.toInt ),
-    "item2" -> SHtml.text("", x => b = x.toInt ),
-    "submit" -> SHtml.submit("submit", () => S.warning("show the user 'good' or 'bad'")))
-  }
-
-
 }
